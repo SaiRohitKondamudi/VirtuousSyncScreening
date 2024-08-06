6e32a0b33739b3729b251f4932b9ff8b85e24758
Sai Rohit <rrrohit61@gmail.com>
2024-08-05 21:17:20 -0500

    Removing API Key

--- Sync/Program.cs
+++ Sync/Program.cs
@@ -55,30 +55,16 @@ namespace Sync
 
         private static async Task Sync()
         {
-            var apiKey = "v_eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiN2VhYTBhNTQtYTBiZC00OTNlLWFjNDMtZjNjZGEwZmVlNWQ5IiwiZXhwIjoyMTQ3NDgzNjQ3LCJpc3MiOiJodHRwczovL2FwcC52aXJ0dW91c3NvZnR3YXJlLmNvbSIsImF1ZCI6Imh0dHBzOi8vYXBpLnZpcnR1b3Vzc29mdHdhcmUuY29tIn0.oN0bfmYMS7lPxGtVH3ouEVhD0Kuzoqa2nAnuvPTyPpk";
+            var apiKey = "REPLACE_WITH_API_KEY_PROVIDED";
             var configuration = new Configuration(apiKey);
             var virtuousService = new VirtuousService(configuration);
 
             var skip = 0;
             var take = 100;
             var maxContacts = 1000;
-            //var hasMore = true;
 
             await SaveToDatabase(virtuousService, skip, take, maxContacts, configuration);
 
-            //using (var writer = new StreamWriter($"Contacts_{DateTime.Now:MM_dd_yyyy}.csv"))
-            //using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
-            //{
-            //    do
-            //    {
-            //        var contacts = await virtuousService.GetContactsAsync(skip, take);
-            //        csv.WriteRecords(contacts.List);
-            //        await SaveToDatabase(virtuousService, skip, take, maxContacts);
-            //        skip += take;
-            //        hasMore = skip >= maxContacts;
-            //    }
-            //    while (!hasMore);
-            //}
         }
 
 
